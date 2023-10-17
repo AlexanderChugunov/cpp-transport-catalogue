1541a6b67b1a09cb8785d08531038d7e207170db
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-10-17 11:22:37 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cmath>
-const double R = 6371000;
+const double EARTH_RADIUS = 6371000;
 struct Coordinates {
     double lat;
     double lng;
@@ -21,5 +21,5 @@ inline double ComputeDistance(Coordinates from, Coordinates to) {
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
         + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * R;
+        * EARTH_RADIUS;
 }
