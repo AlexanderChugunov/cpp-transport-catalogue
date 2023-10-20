58f17bf8dbdebe36db8b897088962e03e2157a53
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-10-20 16:04:36 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,1 +1,32 @@
 
+#pragma once
+
+#include "geo.h"
+
+#include <string>
+#include <vector>
+#include <set>
+#include <unordered_map>
+
+namespace transport {
+
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+        std::set<std::string> buses_by_stop;
+    };
+
+    struct Bus {
+        std::string number;
+        std::vector<const Stop*> stops;
+        bool is_circle;
+    };
+
+    struct BusInfo {
+        size_t stops_count;
+        size_t unique_stops_count;
+        double route_length;
+        double curvature;
+    };
+
+} 
