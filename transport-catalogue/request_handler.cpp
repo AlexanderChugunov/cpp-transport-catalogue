e69b64c094fdfa4a85b71570f72a13535a328744
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-12-01 18:41:38 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,50 +1,91 @@
 #include "request_handler.h"
 
-std::optional<transport::BusInfo> RequestHandler::GetBusInfo(const std::string_view bus_number) const {
-    transport::BusInfo bus_stat{};
-    const transport::Bus* bus = catalogue_.FindBus(bus_number);
 
-    if (!bus) throw std::invalid_argument("bus not found");
-    if (bus->is_circle) bus_stat.stops_count = bus->stops.size();
-    else bus_stat.stops_count = bus->stops.size() * 2 - 1;
-
-    int route_length = 0;
-    double geographic_length = 0.0;
-
-    for (size_t i = 0; i < bus->stops.size() - 1; ++i) {
-        auto from = bus->stops[i];
-        auto to = bus->stops[i + 1];
-        if (bus->is_circle) {
-            route_length += catalogue_.GetDistance(from, to);
-            geographic_length += geo::ComputeDistance(from->coordinates,
-                to->coordinates);
-        }
-        else {
-            route_length += catalogue_.GetDistance(from, to) + catalogue_.GetDistance(to, from);
-            geographic_length += geo::ComputeDistance(from->coordinates,
-                to->coordinates) * 2;
-        }
-    }
-
-    bus_stat.unique_stops_count = catalogue_.UniqueStopsCount(bus_number);
-    bus_stat.route_length = route_length;
-    bus_stat.curvature = route_length / geographic_length;
-
-    return bus_stat;
+RequestHandler::RequestHandler( transport_catalogue::TransportCatalogue& db,  renderer::MapRenderer& renderer):
+	db_(db),
+	renderer_(renderer)
+{
 }
 
-const std::set<std::string> RequestHandler::GetBusesByStop(std::string_view stop_name) const {
-    return catalogue_.FindStop(stop_name)->buses_by_stop;
+// Возвращает информацию о маршруте (запрос Bus)
+std::shared_ptr<transport_catalogue::Bus> RequestHandler::GetBusStat(const std::string_view& bus_name) const {
+	return db_.FindBus(bus_name);
 }
 
-bool RequestHandler::IsBusNumber(const std::string_view bus_number) const {
-    return catalogue_.FindBus(bus_number);
+// Возвращает маршруты, проходящие через
+ std::set<std::string_view> RequestHandler::GetBusesByStop(const std::string_view& stop_name) const {
+	return db_.GetStopInfo(stop_name);
 }
 
-bool RequestHandler::IsStopName(const std::string_view stop_name) const {
-    return catalogue_.FindStop(stop_name);
-}
+
 
 svg::Document RequestHandler::RenderMap() const {
-    return renderer_.GetSVG(catalogue_.GetSortedAllBuses());
+	return renderer_.GetDocument();
+}
+
+
+void RequestHandler::BuildMap() {
+	
+	auto name_ = db_.GetBusName();
+	
+	std::set<std::string> stop_name;
+	
+	if (!name_.empty()) {
+		std::vector <geo::Coordinates> all_coordinates;
+		std::vector <std::vector<geo::Coordinates>> coordinates;
+		std::map<std::string, geo::Coordinates> stopname_to_cooord;
+		for (auto& name : name_) {
+			auto bus = db_.FindBus(name);
+			if (!bus.get()->route.empty()) {
+				std::vector <geo::Coordinates> route_coordinates;
+
+				for (auto stop_ : bus.get()->route) {
+
+					route_coordinates.push_back(stop_.get()->coordinates);
+					all_coordinates.push_back(stop_.get()->coordinates);
+					if (stopname_to_cooord.find(stop_.get()->name) == stopname_to_cooord.end()) {
+						stopname_to_cooord[stop_.get()->name] = stop_.get()->coordinates;
+					}
+				}
+				coordinates.push_back(route_coordinates);
+			}
+		}
+
+
+		renderer::SphereProjector proj_ = renderer_.AddRoute(all_coordinates, coordinates);
+
+
+
+
+
+		BuildBusName(proj_);
+		BuildStops(stopname_to_cooord, proj_);
+	}
+
+}
+void RequestHandler::BuildBusName(renderer::SphereProjector proj_) {
+	auto names = db_.GetBusName();
+	if (!names.empty()) {
+		int start = 0;
+		for (auto name : names) {
+			
+			auto bus = db_.FindBus(name);
+			if (!bus.get()->route.empty()) {
+				if (bus.get()->is_looped) {
+					renderer_.AddBusName(bus.get()->name, bus.get()->route[0].get()->coordinates, bus.get()->route[0].get()->coordinates, proj_,start);
+				}
+				else {
+					int size_ = bus.get()->route.size();
+					renderer_.AddBusName(bus.get()->name, bus.get()->route[0].get()->coordinates, bus.get()->route[size_/2].get()->coordinates, proj_,start);
+				}
+				start++;
+			}
+		}
+	}
+	
+}
+
+void RequestHandler::BuildStops(std::map<std::string, geo::Coordinates>& stops, renderer::SphereProjector proj_) {
+	renderer_.AddStop(stops, proj_);
+
 }
