1ce3a1aec6d586d33cf030c85cdcb2ffcb5aa2ea
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-12-03 16:16:20 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -5,15 +5,24 @@
 
 namespace geo {
 
-double ComputeDistance(Coordinates from, Coordinates to) {
+    bool Coordinates::operator==(const Coordinates& other) const {
+        return lat == other.lat && lng == other.lng;
+    }
+    bool Coordinates::operator!=(const Coordinates& other) const {
+        return !(*this == other);
+    }
+
+    double ComputeDistance(Coordinates from, Coordinates to) {
     using namespace std;
+    static const double earth_radius = 6371000.0;
     if (from == to) {
         return 0;
     }
     static const double dr = M_PI / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
-}
+        * earth_radius;
+    }
+
 
 }  // namespace geo
