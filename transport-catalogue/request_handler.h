a458a5b57951c2bad0e01af6dd6111a2884fcebc
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-12-03 16:18:29 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,34 +1,35 @@
 #pragma once
-
-#include"transport_catalogue.h"
-#include"map_renderer.h"
-
-
+#include "transport_catalogue.h"
+#include "json.h"
+#include "map_renderer.h"
 
 
 class RequestHandler {
 public:
+    RequestHandler() = default;
+
+    //RequestHandler(transport_base_processing::TransportCatalogue& base) : db_(base) {
+    //    
+    //}
+
+    void JasonStatRequest(const json::Array*, std::ostream& out);
+    
     // MapRenderer понадобится в следующей части итогового проекта
-    RequestHandler(transport_catalogue::TransportCatalogue& db, renderer::MapRenderer& renderer);
+    RequestHandler(const transport_base_processing::TransportCatalogue& db, const renderer::MapRenderer& renderer) : db_(db), renderer_(renderer) {
 
-    
-    std::shared_ptr<transport_catalogue::Bus> GetBusStat(const std::string_view& bus_name) const;
+    }
 
-    
-    std::set<std::string_view> GetBusesByStop(const std::string_view& stop_name) const;
+    // Возвращает информацию о маршруте (запрос Bus)
+    std::optional<transport_base_processing::BusInfo> GetBusStat(const std::string_view& bus_name) const;
 
+    // Возвращает маршруты, проходящие через
+    const std::set<std::string>* GetBusesByStop(const std::string_view& stop_name) const;
+
+    // Этот метод будет нужен в следующей части итогового проекта
     svg::Document RenderMap() const;
 
-    void BuildMap();
-
-    
-   
 private:
-    transport_catalogue::TransportCatalogue &db_;
-    renderer::MapRenderer &renderer_;
-
-
-    void BuildBusName(renderer::SphereProjector proj_);
-
-    void BuildStops(std::map<std::string, geo::Coordinates>& stops, renderer::SphereProjector proj_);
+    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
+    const transport_base_processing::TransportCatalogue& db_;
+    const renderer::MapRenderer& renderer_;
 };
