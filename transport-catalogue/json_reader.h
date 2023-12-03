1e0a65ef3cdefcd6e49f2cdacae081782dbea1fc
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-12-03 16:17:27 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,68 +1,32 @@
 #pragma once
-#include <iostream>
-#include <sstream>
-#include <deque>
-#include <unordered_map>
-#include <string_view>
-#include <algorithm>
-#include <tuple>
-#include <iomanip>
-#include <deque>
 #include "transport_catalogue.h"
 #include "json.h"
 #include "map_renderer.h"
-#include "svg.h"
-#include "request_handler.h"
-enum class RequestType {
-    AddStop,
-    AddBus,
-	GetInfo,
-    GetBusInfo,
-    GetStopInfo,
-	GetMap
-};
-struct AddStopRequest
-{
-	std::string name;
-	geo::Coordinates coord;
-	json::Dict stops_around;
 
-};
-struct AddBusRequest
-{
-	std::string name;
-	json::Array route;
-	bool is_loop;
-};
+namespace json {
+	using StopDistancesInfo = transport_base_processing::TransportCatalogue::StopDistancesInfo;
+	using JsonStopDistanceMap = std::unordered_map<std::string, std::unordered_map<std::string, long unsigned int>>;
+	class JsonBaseProcessing {
+		public:
+		JsonBaseProcessing(Document document) : document_(document) {
+		}
 
-struct GetInfo{
-	RequestType type;
-	int id;
+		void CreateBase(transport_base_processing::TransportCatalogue& base);
+
+		const renderer::RenderSettings& GetRenderSet() const; 
+
+		const Array* GetStatRequest() const;
 	
-	std::string name;
-};
+	private:
+		Document document_;
+		JsonStopDistanceMap stop_to_stop_distances;
+		renderer::RenderSettings render_settings_;
 
-
-using RequestMap = std::map<RequestType, std::vector<std::variant<AddStopRequest, AddBusRequest, GetInfo>>>;
-using namespace std::literals;
-
-
-RequestMap& ParseAddRequst(RequestMap& requsts, const json::Node& node);
-
-RequestMap& ParseGetRequst(RequestMap& requsts, const json::Node& node);
-
-RequestMap ParseJson(std::istream& input, renderer::MapRenderer& map_for_setting);
-
-json::Dict BuildGetBusAnswer(transport_catalogue::TransportCatalogue& catalogue, GetInfo request);
-
-json::Dict BuildGetStopAnswer(transport_catalogue::TransportCatalogue& catalogue, GetInfo request);
-
-transport_catalogue::TransportCatalogue& ProcesAddRequest(RequestMap& requests, transport_catalogue::TransportCatalogue& catalogue);
-
-json::Document ProcesGetRequest(RequestMap& requests, transport_catalogue::TransportCatalogue& catalogue, renderer::MapRenderer& map);
-
-svg::Color ParseColor(const json::Node& color_);
-
-renderer::MapRenderer ParseRenderSetting(const json::Dict& node, renderer::MapRenderer& map_for_setting);
-
-void ProcessJsonRequests(std::istream& input, std::ostream& output);
+		svg::Color ParseColor(const Node& color_collection) const;
+		void ParseRenderSettings(const Dict* data);
+		std::vector<transport_base_processing::Stop> ParseStopRequests(const Array* data);
+		std::vector< transport_base_processing::Bus> ParseBusRequests(const Array* data, transport_base_processing::TransportCatalogue& base);
+		StopDistancesInfo ParseStopDistInfo(std::string& stop);
+		
+	};
+}
