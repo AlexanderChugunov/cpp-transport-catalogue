0ad839bc37484dadf21e19231d9a676d88c92665
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-12-12 15:02:18 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -20,16 +20,15 @@ json::Document LoadJSON(const std::string& s) {
 */
 int main() {
 	using namespace std;
-	using namespace svg;
-
-	transport_base_processing::TransportCatalogue base;
+    using namespace svg;
+    
+    transport_base_processing::TransportCatalogue base;
 	json::Document test = json::Load(std::cin);
-	json::JsonBaseProcessing input_json(test);
+	json::JsonBaseProcessing input_json (test);
 	input_json.CreateBase(base);
-	renderer::MapRenderer map_render;
-	map_render.SetRenderSettings
-	(input_json.GetRenderSet());
-	RequestHandler requests(base, map_render);
-	requests.JasonStatRequest(input_json.GetStatRequest(), std::cout);
-	return 0;
+	transport_base_processing::MapRenderer map_render;
+	map_render.SetRendSet(input_json.GetRenderSet());
+	transport_base_processing::RequestHandler requests(base, map_render);
+	Print(input_json.GetStatRequest(requests), std::cout);
+    return 0;
 }
