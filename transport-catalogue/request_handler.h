003c9eb1bd617050ae0e6c5af58525f54c12cf35
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2024-01-27 14:27:40 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -2,6 +2,7 @@
 #include "transport_catalogue.h"
 #include "json.h"
 #include "map_renderer.h"
+#include "transport_router.h"
 
 namespace transport_base_processing {
     class RequestHandler {
@@ -14,26 +15,33 @@ namespace transport_base_processing {
 
 
         // MapRenderer понадобится в следующей части итогового проекта
-        RequestHandler(const transport_base_processing::TransportCatalogue& db, const transport_base_processing::MapRenderer& renderer) : db_(db), renderer_(renderer) {
+        RequestHandler(const TransportCatalogue& db, const MapRenderer& renderer) : db_(db), renderer_(renderer), db_graf(std::move(TransportGraph(db))){
 
         }
 
         // Возвращает информацию о маршруте (запрос Bus)
-        std::optional<transport_base_processing::BusInfo> GetBusStat(const std::string_view& bus_name) const;
+        std::optional<BusInfo> GetBusStat(const std::string_view& bus_name) const;
 
         // Возвращает маршруты, проходящие через
         const std::set<std::string>* GetBusesByStop(const std::string_view& stop_name) const;
 
+        // Возвращает оптимальный маршрут от остановки from до остановки to
+        std::optional<graph::Router<double>::RouteInfo> BuildRoute(std::string_view from, std::string_view to) const;
+
         const transport_base_processing::TransportCatalogue& GetBase() const;
 
         const transport_base_processing::MapRenderer& GetRenderSet() const;
+
+        const TransportGraph& GetTransportGraph() const;
 
         // Этот метод будет нужен в следующей части итогового проекта
         svg::Document RenderMap() const;
 
     private:
         // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
-        const transport_base_processing::TransportCatalogue& db_;
-        const transport_base_processing::MapRenderer& renderer_;
+        const TransportCatalogue& db_;
+        const MapRenderer& renderer_;
+        TransportGraph db_graf;
+        graph::Router<double> db_router = db_graf.GetRouter();
     };
 } // namespace transport_base_processing
